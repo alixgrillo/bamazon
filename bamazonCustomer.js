307a2efb020b7160f57b8b59683ddfe44b24f15c
Alix Grillo <alix.grillo2+github@gmail.com>
2019-12-24 13:05:46 -0700

    finalized customer ordering logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,14 +10,14 @@ var connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     //console.log(results);
     start();
 });
 
-function start(){
-    connection.query("SELECT * FROM products", function (err, results){
+function start() {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         var table = new Table({
             dafaultOnValue: 0,
@@ -25,57 +25,69 @@ function start(){
             head: ['Product ID', 'Product Name', 'Department', 'Price'],
             colWidths: [20, 25, 25, 25]
         });
-        var prodArr = [];
-        for(var i=0; i<results.length; i++){
+        // var prodArr = [];
+        for (var i = 0; i < results.length; i++) {
             table.push(
-                [results[i].item_id, results[i].product_name, results[i].department_name, "$"+results[i].price]
+                [results[i].item_id, results[i].product_name, results[i].department_name, "$" + results[i].price]
             );
-            prodArr.push({
-                id: results[i].item_id,
-                name: results[i].product_name
-            });
-        }
+            // prodArr.push({
+            //     id: results[i].item_id,
+            //     name: results[i].product_name
+        };
+
         console.log(table.toString());
-        sellProduct(prodArr);
+        sellProduct();
     });
 }
 
-function sellProduct(prodArr){
-    inquirer.prompt([
-        {
-            message: "What product would you like to buy?",
-            type: 'list',
-            choices: function(){
-                var choiceArr=[];
-                for (var i =0; i<prodArr.length; i++){
-                    choiceArr.push(prodArr[i].id + " (" + prodArr[i].name + ")");
-                }
-                return choiceArr;
-            },
-            name: 'product'
-        },
-        {
-            message: "How many would you like to buy?",
-            type: 'input',
-            name: 'quantity'
-        }
-    ]).then(function(answer){
-        var prodID = answer.product.slice(0,5);
-        var prodQuantity = 0;
-        connection.query('SELECT * FROM products WHERE item_id = ?', [prodID], function(err, result){
-            if (err) throw err;
-            prodQuantity = result[0].stock_quantity;
-            if(prodQuantity - answer.quantity < 0){
-                console.log("Insufficient Quantity");
-                start();
-            } else {
-                connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', 
-                [prodQuantity-answer.quantity, prodID], function (err, result){
-                    if(err) throw err;
-                    console.log("Purchase successful");
-                    start();
-                });
+function sellProduct() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                message: "What product would you like to buy?",
+                type: 'list',
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].item_id + " (" + results[i].product_name + ")");
+                    }
+                    choiceArray.push("Stop Shopping!");
+                    return choiceArray;
+                },
+                name: 'product'
             }
+        ]).then(function (answer) {
+            var prodID = answer.product.slice(0, 5);
+            var prodQuantity = 0;
+            if (answer.product === "Stop Shopping!") {
+                connection.end();
+                return
+            }
+            inquirer.prompt([
+                {
+                    message: "How many would you like to buy?",
+                    type: 'input',
+                    name: 'quantity'
+                }
+            ]).then(function (quantityAnswer) {
+                connection.query('SELECT * FROM products WHERE item_id = ?', [prodID], function (err, result) {
+                    if (err) throw err;
+                    prodQuantity = result[0].stock_quantity;
+                    if (prodQuantity - quantityAnswer.quantity < 0) {
+                        console.log("Insufficient Quantity");
+                        sellProduct();
+                    } else {
+                        connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?',
+                            [prodQuantity - quantityAnswer.quantity, prodID], function (err, result) {
+                                if (err) throw err;
+                                console.log("Purchase successful");
+                                sellProduct();
+                        });
+                    }
+                })
+            })
         })
     })
+
 }
